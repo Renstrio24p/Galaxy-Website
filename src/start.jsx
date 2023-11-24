a3b2fc8b06ad53da78ef572ce63f3503c759a922
Waren Gador <waren.gador15@gmail.com>
2023-11-24 16:43:09 +0800

    add files

--- src/start.jsx
+++ src/start.jsx
@@ -4,7 +4,7 @@ import './assets/css/app.css'
 export default function Start(){
     return (
         <div>
-           <h1>Hello</h1>
+           <h1>Hello 1</h1>
             {/* You can start coding this file */}
         </div>
     )
